c1d428b748dedf8c4c8e05717772dff6c8237e0d
NurdinDev <nourba691@gmail.com>
2020-08-16 23:36:57 +0300

    movie api and api error handler

--- backend/app/model/movie.py
+++ backend/app/model/movie.py
@@ -1,50 +1,48 @@
 import datetime
 from .. import db
 
+
 class Movie(db.Model):
-  """
-  Movie Model
-  """
+	__tablename__ = 'movies'
 
-  # table name
-  __tablename__ = 'movies'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(), nullable=False)
+	created_at = db.Column(db.DateTime)
+	modified_at = db.Column(db.DateTime)
 
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(), nullable=False)
-  created_at = db.Column(db.DateTime)
-  modified_at = db.Column(db.DateTime)
+	# class constructor
+	def __init__(self, name):
+		self.name = name
+		self.created_at = datetime.datetime.utcnow()
+		self.modified_at = datetime.datetime.utcnow()
 
-  #class constructor
-  def __init__(self, data):
-    """
-    Class constructor
-    """
-    self.name = data.get('name')
-    self.created_at = datetime.datetime.utcnow()
-    self.modified_at = datetime.datetime.utcnow()
+	def save(self):
+		db.session.add(self)
+		db.session.commit()
 
-  def save(self):
-    db.session.add(self)
-    db.session.commit()
+	def update(self, data):
+		for key, item in data.items():
+			setattr(self, key, item)
+		self.modified_at = datetime.datetime.utcnow()
+		db.session.commit()
 
-  def update(self, data):
-    for key, item in data.items():
-      setattr(self, key, item)
-    self.modified_at = datetime.datetime.utcnow()
-    db.session.commit()
+	def delete(self):
+		db.session.delete(self)
+		db.session.commit()
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+	def format(self):
+		return {
+			'id': self.id,
+			'name': self.name
+		}
 
-  @staticmethod
-  def get_all_movies():
-    return Movie.query.all()
+	@staticmethod
+	def get_all_movies():
+		return Movie.query.all()
 
-  @staticmethod
-  def get_one_movie(id):
-    return Movie.query.get(id)
+	@staticmethod
+	def get_one_movie(id):
+		return Movie.query.get(id)
 
-  
-  def __repr(self):
-    return '<id {}>'.format(self.id)
+	def __repr(self):
+		return '<id {}>'.format(self.id)
