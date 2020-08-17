d32e91ea8b48d2a25bc8680fc82d5f282a580f79
NurdinDev <nourba691@gmail.com>
2020-08-17 09:12:40 +0300

    add some actor api test

--- backend/app/test/test_api.py
+++ backend/app/test/test_api.py
@@ -1,9 +1,29 @@
-import unittest
 from app.test.base import BaseTestCase
 import json
 
+
 class ApiTestCase(BaseTestCase):
-  def test_get_movies(self):
-    res = self.client.get('/')
-    data = json.loads(res.data)
-    self.assertEqual(res.status_code, 200)
+
+	def setUp(self):
+		self.new_movie = {
+			'name': "new movie"
+		}
+
+	def test_index(self):
+		res = self.client.get('/')
+		data = json.loads(res.data)
+		self.assert200(res)
+		self.assertEqual(data['success'], True)
+
+	def test_get_movies(self):
+		res = self.client.get('/movies')
+		data = json.loads(res.data)
+		self.assert200(res)
+		self.assertEqual(data['success'], True)
+
+	def test_post_movie(self):
+		res = self.client.post('/movies', json=self.new_movie)
+		data = json.loads(res.data)
+		self.assert200(res)
+		self.assertEqual(data['success'], True)
+		# self.assertTrue(len(data['movies']))
