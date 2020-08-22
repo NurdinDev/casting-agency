31a23f23017428a56967d0d4b71ee4f07c135f55
NurdinDev <nureddinbedevi@gmail.com>
2020-08-22 05:22:16 +0300

    CRUD in movie page

--- frontend/src/pages/Account.tsx
+++ frontend/src/pages/Account.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Account.css';
 
 const Account: React.FC = () => {
   return (
